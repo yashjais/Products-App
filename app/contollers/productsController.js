c945681aae7fb46447ed7518a8cd58d0caf0b81c
Yash Jaiswal <jais.yash01@gmail.com>
2020-04-01 18:37:17 +0530

    discounted_products_count|avg_discount && brand.name

--- app/contollers/productsController.js
+++ app/contollers/productsController.js
@@ -21,10 +21,10 @@ module.exports.create = (req, res) => {
 
 module.exports.query = (req, res) => {
     const body = req.body
+    const operand1 = body.filters[0].operand1
+    let operand2 = body.filters[0].operand2
+    const operator = body.filters[0].operator
     if(body.query_type == 'discounted_products_list') {
-        const operand1 = body.filters[0].operand1
-        let operand2 = body.filters[0].operand2
-        const operator = body.filters[0].operator
         // if(operand1 && operator && operand2){ // checking if coming filter is truthy value or not // coz server side authentication is necessary.}
         if(operand1 == 'brand.name' && operand2) { // checking operands are valid or not
             if(operator == '==') { // checking for the operator is '==' or not for 'brand.name'
@@ -44,34 +44,37 @@ module.exports.query = (req, res) => {
             }
         } else if(operand1 == "discount") {
             operand2 = Number(operand2) // checking if the operand 2 is a valid number
-            if(operand2){
+            if(operand2 || operand2 == '0'){
                 if(operator == '=='){
                     Product.aggregate([
                         { $project: {'price.regular_price.value': 1, difference: {$subtract: ["$price.regular_price.value", "$price.offer_price.value"]}} },
                         { $project: {discount: {$divide: ["$difference", "$price.regular_price.value"]}} },
+                        { $project: {discount: {$multiply: ["$discount", 100]}} },
                         { $match: {discount: { $eq: operand2}} },
                         { $limit: 10 }
                       ]).
-                      then(products => res.send(products))
-                      .catch(err => res.send(err))
+                        then(products => res.send(products))
+                        .catch(err => res.send(err))
                 } else if(operator == '>'){
                     Product.aggregate([
                         { $project: {'price.regular_price.value': 1, difference: {$subtract: ["$price.regular_price.value", "$price.offer_price.value"]}} },
                         { $project: {discount: {$divide: ["$difference", "$price.regular_price.value"]}} },
+                        { $project: {discount: {$multiply: ["$discount", 100]}} },
                         { $match: {discount: { $gt: operand2}} },
                         { $limit: 10 }
                       ]).
-                      then(products => res.send(products))
-                      .catch(err => res.send(err))
+                        then(products => res.send(products))
+                        .catch(err => res.send(err))
                 } else if(operator == '<'){
                     Product.aggregate([
                         { $project: {'price.regular_price.value': 1, difference: {$subtract: ["$price.regular_price.value", "$price.offer_price.value"]}} },
                         { $project: {discount: {$divide: ["$difference", "$price.regular_price.value"]}} },
+                        { $project: {discount: {$multiply: ["$discount", 100]}} },
                         { $match: {discount: { $lt: operand2}} },
                         { $limit: 10 }
                       ]).
-                      then(products => res.send(products))
-                      .catch(err => res.send(err))
+                        then(products => res.send(products))
+                        .catch(err => res.send(err))
                 } else {
                     res.send('operator is not valid')
                 }
@@ -79,9 +82,34 @@ module.exports.query = (req, res) => {
                 res.send('operand 2 is not a number')
             }
         } else{
-            res.send('operand 1 is not valid')
+            res.send('operands are not valid')
         }
-    } else {x
+    } else if(body.query_type == 'discounted_products_count|avg_discount') {
+        if(operand1 == 'brand.name' && operand2) { 
+            if(operator == '==') { 
+                Product.aggregate([
+                    { $match: {'brand.name': operand2 } },
+                    { $project: {'price.regular_price.value': 1, difference: {$subtract: ["$price.regular_price.value", "$price.offer_price.value"]}} },
+                    { $project: {discount: {$divide: ["$difference", "$price.regular_price.value"]}} },
+                    { $project: {discount: {$multiply: ["$discount", 100]}} },
+                    // { $group: { _id: null, myCount: { $sum: 1 } } },
+                    // { $project: { _id: 0 } }
+                    // { $group: { _id: null, total: {$sum: "$discount"}} },
+                    { $group: { _id: null, total: {$sum: "$discount"}, discounted_products_count: { $sum: 1 }} },
+                    { $project: { discounted_products_count: 1, avg_discount: {$divide: ["$total", "$discounted_products_count"]}} },
+                    // { $project: {discount: 1, total: 1, count: {$sum: ["$discount"]}} },
+                    // { $match: {discount: { $eq: operand2}} },
+                    { $limit: 500 }
+                  ]).
+                    then(products => res.send(products))
+                    .catch(err => res.send(err))
+            } else{
+                res.send('operator not valid')
+            }
+        } else{
+            res.send('operands are not valid')
+        }
+    }else {
         res.send('invalid request')
     }
 }
--- playground.js/obj.js
+++ playground.js/obj.js
@@ -377,3 +377,381 @@ const obj = {
     "price_positioning": 0,
     "price_positioning_text": "expensive"
 },
+
+
+
+const obj2 = {
+    "brand": {
+        "name": "prada",
+        "sub_brand": ""
+    },
+    "media": {
+        "standard": [
+            {
+                "order": "1",
+                "url": "http://cache.net-a-porter.com/images/products/1000995/1000995_in_pp.jpg"
+            },
+            {
+                "order": "2",
+                "url": "http://cache.net-a-porter.com/images/products/1000995/1000995_ou_pp.jpg"
+            },
+            {
+                "order": "3",
+                "url": "http://cache.net-a-porter.com/images/products/1000995/1000995_fr_pp.jpg"
+            },
+            {
+                "order": "4",
+                "url": "http://cache.net-a-porter.com/images/products/1000995/1000995_bk_pp.jpg"
+            },
+            {
+                "order": "5",
+                "url": "http://cache.net-a-porter.com/images/products/1000995/1000995_cu_pp.jpg"
+            },
+            {
+                "order": "6",
+                "url": "http://cache.net-a-porter.com/images/products/1000995/1000995_ou_pp.jpg"
+            }
+        ],
+        "thumbnail": [
+            {
+                "order": "1",
+                "url": "https://cache.net-a-porter.com/images/products/1000995/1000995_in_xs.jpg"
+            }
+        ]
+    },
+    "price": {
+        "offer_price": {
+            "currency": "GBP",
+            "value": 855
+        },
+        "regular_price": {
+            "currency": "GBP",
+            "value": 855
+        },
+        "basket_price": {
+            "value": 855,
+            "currency": "GBP"
+        }
+    },
+    "stock": {
+        "available": true
+    },
+    "meta": {
+        "bert_original_classification": {
+            "l1": "clothing",
+            "l2": "bottomwear",
+            "l3": "pants",
+            "l4": ""
+        },
+        "breadcrumbs": {
+            "1": "Clothing ",
+            "2": " Pants ",
+            "3": " Flared"
+        },
+        "reference": ""
+    },
+    "classification": {
+        "l1": "clothing",
+        "l2": "bottomwear",
+        "l3": "pants",
+        "l4": ""
+    },
+    "similar_products": {
+        "meta": {
+            "min_price": {
+                "regular": 855,
+                "offer": 513,
+                "basket": 518
+            },
+            "max_price": {
+                "regular": 855,
+                "offer": 513,
+                "basket": 518
+            },
+            "avg_price": {
+                "regular": 855,
+                "offer": 513,
+                "basket": 518
+            },
+            "total_results": 1,
+            "avg_discount": 0.4
+        },
+        "website_results": {
+            "5da94f4e6d97010001f81d72": {
+                "meta": {
+                    "total_results": 0,
+                    "min_price": {
+                        "regular": 0,
+                        "offer": 0,
+                        "basket": 0
+                    },
+                    "max_price": {
+                        "regular": 0,
+                        "offer": 0,
+                        "basket": 0
+                    },
+                    "avg_price": {
+                        "regular": 0,
+                        "offer": 0,
+                        "basket": 0
+                    },
+                    "avg_discount": 0
+                },
+                "knn_items": []
+            },
+            "5da94f270ffeca000172b12e": {
+                "meta": {
+                    "total_results": 0,
+                    "min_price": {
+                        "regular": 0,
+                        "offer": 0,
+                        "basket": 0
+                    },
+                    "max_price": {
+                        "regular": 0,
+                        "offer": 0,
+                        "basket": 0
+                    },
+                    "avg_price": {
+                        "regular": 0,
+                        "offer": 0,
+                        "basket": 0
+                    },
+                    "avg_discount": 0
+                },
+                "knn_items": []
+            },
+            "5d0cc7b68a66a100014acdb0": {
+                "meta": {
+                    "total_results": 1,
+                    "min_price": {
+                        "regular": 855,
+                        "offer": 513,
+                        "basket": 518
+                    },
+                    "max_price": {
+                        "regular": 855,
+                        "offer": 513,
+                        "basket": 518
+                    },
+                    "avg_price": {
+                        "regular": 855,
+                        "offer": 513,
+                        "basket": 518
+                    },
+                    "avg_discount": 0.4
+                },
+                "knn_items": [
+                    {
+                        "_index": "combined_products_vectorized_ranknet_3ir_nlp",
+                        "_type": "_doc",
+                        "_id": "5d49dc48a3ecbbd90d64db17",
+                        "_score": 1,
+                        "_source": {
+                            "name": "printed high-waisted trousers",
+                            "url": "https://www.farfetch.com/uk/shopping/women/prada-printed-high-waisted-trousers-item-12591980.aspx?storeid=9306",
+                            "brand": {
+                                "name": "prada",
+                                "sub_brand": ""
+                            },
+                            "media": {
+                                "standard": [
+                                    {
+                                        "order": 1,
+                                        "url": "https://cdn-images.farfetch-contents.com/12/59/19/80/12591980_12080084_600.jpg"
+                                    },
+                                    {
+                                        "order": 2,
+                                        "url": "https://cdn-images.farfetch-contents.com/12/59/19/80/12591980_12080085_600.jpg"
+                                    },
+                                    {
+                                        "order": 3,
+                                        "url": "https://cdn-images.farfetch-contents.com/12/59/19/80/12591980_12080086_600.jpg"
+                                    },
+                                    {
+                                        "order": 4,
+                                        "url": "https://cdn-images.farfetch-contents.com/12/59/19/80/12591980_12080087_600.jpg"
+                                    },
+                                    {
+                                        "order": 5,
+                                        "url": "https://cdn-images.farfetch-contents.com/12/59/19/80/12591980_12080090_600.jpg"
+                                    }
+                                ],
+                                "thumbnail": [
+                                    {
+                                        "order": 1,
+                                        "url": "https://cdn-images.farfetch-contents.com/12/59/19/80/12591980_12080084_80.jpg"
+                                    },
+                                    {
+                                        "order": 2,
+                                        "url": "https://cdn-images.farfetch-contents.com/12/59/19/80/12591980_12080085_80.jpg"
+                                    },
+                                    {
+                                        "order": 3,
+                                        "url": "https://cdn-images.farfetch-contents.com/12/59/19/80/12591980_12080086_80.jpg"
+                                    },
+                                    {
+                                        "order": 4,
+                                        "url": "https://cdn-images.farfetch-contents.com/12/59/19/80/12591980_12080087_80.jpg"
+                                    },
+                                    {
+                                        "order": 5,
+                                        "url": "https://cdn-images.farfetch-contents.com/12/59/19/80/12591980_12080090_80.jpg"
+                                    }
+                                ],
+                                "large": [
+                                    {
+                                        "order": 1,
+                                        "url": "https://cdn-images.farfetch-contents.com/12/59/19/80/12591980_12080084_480.jpg"
+                                    },
+                                    {
+                                        "order": 2,
+                                        "url": "https://cdn-images.farfetch-contents.com/12/59/19/80/12591980_12080085_480.jpg"
+                                    },
+                                    {
+                                        "order": 3,
+                                        "url": "https://cdn-images.farfetch-contents.com/12/59/19/80/12591980_12080086_480.jpg"
+                                    },
+                                    {
+                                        "order": 4,
+                                        "url": "https://cdn-images.farfetch-contents.com/12/59/19/80/12591980_12080087_480.jpg"
+                                    },
+                                    {
+                                        "order": 5,
+                                        "url": "https://cdn-images.farfetch-contents.com/12/59/19/80/12591980_12080090_480.jpg"
+                                    }
+                                ]
+                            },
+                            "meta": {
+                                "breadcrumbs": {
+                                    "1": "home",
+                                    "2": "Women",
+                                    "3": "Prada",
+                                    "4": "Clothing",
+                                    "5": "High-Waisted Trousers",
+                                    "6": "printed high-waisted trousers"
+                                },
+                                "gender": "Woman",
+                                "bert_original_classification": {
+                                    "l1": "clothing",
+                                    "l2": "bottomwear",
+                                    "l3": "pants",
+                                    "l4": "skinny pants"
+                                }
+                            },
+                            "stock": {
+                                "available": true,
+                                "units": 1,
+                                "text": ""
+                            },
+                            "sku": 12591980,
+                            "description_text": "Confident yet considered, iconic Italian fashion house Prada balances  uncompromised quality with experimental, season-defining styles. Made in  Italy from silk, these black, red and white trousers feature a novelty  print, a high waist and a flared style. ",
+                            "website_id": "5d0cc7b68a66a100014acdb0",
+                            "price": {
+                                "offer_price": {
+                                    "currency": "GBP",
+                                    "value": 513
+                                },
+                                "regular_price": {
+                                    "currency": "GBP",
+                                    "value": 855
+                                },
+                                "basket_price": {
+                                    "value": 518,
+                                    "currency": "GBP"
+                                }
+                            },
+                            "sizes": [
+                                "46"
+                            ],
+                            "spider": "farfetch_gb_pv",
+                            "classification": {
+                                "l1": "clothing",
+                                "l2": "bottomwear",
+                                "l3": "pants",
+                                "l4": "skinny pants"
+                            },
+                            "created_at": "2019-08-06T20:00:08.778Z",
+                            "updated_at": "2019-12-03T06:15:02.446Z",
+                            "lv_url": "https://www.farfetch.com/uk/shopping/women/sale/clothing-1/items.aspx?page=143&view=180",
+                            "positioning": {
+                                "page": 143,
+                                "rank": 159
+                            }
+                        }
+                    }
+                ]
+            },
+            "5da94ef80ffeca000172b12c": {
+                "meta": {
+                    "total_results": 0,
+                    "min_price": {
+                        "regular": 0,
+                        "offer": 0,
+                        "basket": 0
+                    },
+                    "max_price": {
+                        "regular": 0,
+                        "offer": 0,
+                        "basket": 0
+                    },
+                    "avg_price": {
+                        "regular": 0,
+                        "offer": 0,
+                        "basket": 0
+                    },
+                    "avg_discount": 0
+                },
+                "knn_items": []
+            },
+            "5da94e940ffeca000172b12a": {
+                "meta": {
+                    "total_results": 0,
+                    "min_price": {
+                        "regular": 0,
+                        "offer": 0,
+                        "basket": 0
+                    },
+                    "max_price": {
+                        "regular": 0,
+                        "offer": 0,
+                        "basket": 0
+                    },
+                    "avg_price": {
+                        "regular": 0,
+                        "offer": 0,
+                        "basket": 0
+                    },
+                    "avg_discount": 0
+                },
+                "knn_items": []
+            }
+        }
+    },
+    "positioning": {
+        "page": 133,
+        "rank": 35
+    },
+    "sizes": [],
+    "created_at": "2019-10-21T00:00:00.000Z",
+    "_id": "5da8221a6504fb65da005a7b",
+    "name": "Prada  Printed silk crepe de chine flared pants   ",
+    "sku": "1000995",
+    "url": "https://www.net-a-porter.com/gb/en/product/1000995/prada/printed-silk-crepe-de-chine-flared-pants",
+    "website_id": "5da6d40110309200045874e6",
+    "description_text": "Black, red and cream silk crepe de chine Concealed button, hook and zip fastening at front  100% silk  Dry clean  Made in Italy",
+    "spider": "netaporter_gb_pv",
+    "updated_at": "2020-01-18T06:07:49.170Z",
+    "lv_url": "",
+    "price_changes": [
+        {
+            "date": "2019-10-21T00:00:00.000Z",
+            "previous_offer_price": 855,
+            "current_offer_price": 855,
+            "previous_regular_price": 855,
+            "current_regular_price": 855
+        }
+    ],
+    "price_positioning": 100,
+    "price_positioning_text": "cheaper"
+},
