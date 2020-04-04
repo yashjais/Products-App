539274418d82fdb8cbb55016e53d14af1ec77537
Yash Jaiswal <jais.yash01@gmail.com>
2020-04-04 17:24:29 +0530

    code refactoring

--- config/database.js
+++ config/database.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose')
 
 const setUPDb = () => {
-    mongoose.connect('mongodb://localhost:27017/greendeck-task',  {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
-        .then(res => console.log('connected to the db'))
-        .catch(err => console.log(err))
+    mongoose.connect('mongodb+srv://yashjais:Pengu123@cluster0-wz7f8.mongodb.net/Products?retryWrites=true&w=majority',  { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify : false, useCreateIndex: true })
+    .then(res => {
+        console.log('connected to db') 
+    })
+    .catch(err => {
+        console.log(err)
+    })
 }
 
 module.exports = setUPDb
--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require('express')
-const setUPDb = require('./config/database')
+const setUpDb = require('./config/database')
 const router = require('./config/routes')
 const cors = require('cors')
+const path = require('path') 
 
 const app = express()
-const port = 3010
+const port = process.env.PORT || 3010; 
+
+setUpDb()
 
 app.use(express.json())
 app.use(cors())
 app.use('/', router)
 
-setUPDb()
+app.use(express.static(path.join(__dirname,"client/build"))) 
+app.get("*",(req,res) => { 
+    res.sendFile(path.join(__dirname + "/client/build/index.html")) 
+}) 
 
 app.listen(port, () => {
     console.log('listening on the port', port)
